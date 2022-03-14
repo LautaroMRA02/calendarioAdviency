a0f9536e0bc6ca6e133b6d91a4705a33228c8638
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-03-14 00:15:31 -0300

    (dia_4):  #terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import {React,useState} from 'react'
 import './App.css';
-// import { Grid, GridItem } from '@chakra-ui/react'
-import { Box,Text,Flex } from '@chakra-ui/react'
+import { nanoid } from 'nanoid';
+import { Box,Text,Flex,FormControl,Button,Input,List,ListItem,Center  } from '@chakra-ui/react'
 function App() {
+  const [regaloItem, setregaloItem] = useState('')
+  const [listaRegalos, setlistaRegalos] = useState([])
+  const Regalos =listaRegalos.map(item => (
+        <ListItem 
+          key={item.id} 
+          id={item.id} 
+          display='flex' 
+          flexWrap='nowrap' 
+          justifyContent='space-between' 
+          alignItems='center' 
+          fontSize={'1.2rem'}
+          fontWeight={'bold'}
+        >
+          <Text>{item.regalo}</Text> 
+          <Button colorScheme={'blue'} size='sm' onClick={(e)=>deleteRegalo(e)}>X</Button>
+        </ListItem>  ))
+        
+
+  function deleteRegalo(e){
+    let id = e.nativeEvent.path[1].id
+    let newArray = [];
+    setlistaRegalos(prevlistaRegalos => {
+      for(let i = 0;i < prevlistaRegalos.length; i++){if(prevlistaRegalos[i].id !== id){newArray.push(prevlistaRegalos[i])};}
+      return newArray
+    })}
+
+  function agregarRegalo(){
+  const useRegalo = {id:nanoid(),regalo:`${regaloItem}`}
+  setlistaRegalos(prevlistaRegalos => [...prevlistaRegalos,useRegalo] )
+  setregaloItem('')}
+  function ListRegalos(){return(<List>{Regalos}</List>)}
   return (
-    <Flex flexDirection="column" >
+    <Flex flexDirection="column" backgroundColor={'white'} p={5} borderRadius={10}>
         <Box>
-        <Text fontSize='2rem'>Regalos:</Text>
-          <ul>
-            <li>
-              <Text>Medias</Text>
-            </li>
-            <li>
-            <Text>Caramelos</Text>
-            </li>
-            <li>
-            <Text>Vitel Tone</Text>
-            </li>
-          </ul>
+        <Text fontSize='3rem' style={{fontFamily:'Mountains of Christmas',fontWeight:700}}>Regalos:</Text>
+        <FormControl pt={4} pb={4} display={'flex'} gap={'8px'} flexWrap={ ['wrap','nowrap','nowrap','nowrap']}>
+          <Input onKeyDown={(e)=>{e.code === 'Enter' && agregarRegalo() }} onChange={(e)=>setregaloItem(e.target.value)}  name='regalo' id='regalo' type={'text'} autoComplete="off" value={regaloItem} />
+          <Button type='submit' onClick={()=>agregarRegalo()} colorScheme={'blue'} width={['100%','30%','30%','30%']}>Agregar</Button>
+        </FormControl>
+        {listaRegalos.length === 0 ? <Center ><Text color='gray.500' isTruncated>Agregar regalo</Text></Center>:  <ListRegalos/>}
         </Box>
     </Flex>
   );
 }
 
 export default App;
+
+
+
+
