8626a0d8d02917476cb8aaa0661ca7478e47c319
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-03-15 00:22:54 -0300

    (dia_5): #terminado

--- src/App.js
+++ src/App.js
@@ -6,30 +6,45 @@ import { Box,Text,Flex,FormControl,Input,Button,List,ListItem, Center } from '@c
 import {nanoid} from 'nanoid'
 function App() {
   const [listRegalos, setlistRegalos] = useState([])
-  const [regaloItem, setRegaloItem] = useState('second')
-  const listachilds = listRegalos.map((item)=><ListItem key={nanoid()}>{item}</ListItem>)
+  const [regaloItem, setRegaloItem] = useState('')
+  const listachilds = listRegalos.map((item)=><ListItem key={item.id} id={item.id}display={'flex'} justifyContent={'space-between'} alignItems={'center'}><Text>{item.regalo}</Text> <Button size='sm' onClick={(e)=>deleteRegalo(e)} colorScheme={'red'}>X</Button></ListItem>)
 
   function agregarRegalo(){
-alert('sadsa')
+    const item = {id: nanoid(),regalo: `${regaloItem}`}
+    setlistRegalos(prevListRegalos => [...prevListRegalos,item])
+    setRegaloItem('')
+    console.log(listRegalos)
+
+  }
+  function deleteRegalo(e){
+    const newLista = [];
+    console.log('sdasd')
+    const id = e.nativeEvent.path[1].id
+    console.log(id)
+
+    setlistRegalos(prevlistRegalos => {
+    for(let i = 0; i < prevlistRegalos.length;i++){ if(prevlistRegalos[i].id !== id){ newLista.push(prevlistRegalos[i])};}
+    return newLista
+  })
   }
   const ListRegalosComponent = () =>{
     return (
-      <List>
-        {listachilds ? <Center><Text>no hay regalos</Text></Center> : listachilds}
+      <List pt={4} pb={4}>
+        {listachilds.length > 0 ?  listachilds:  <Center><Text>no hay regalos</Text></Center>}
       </List>
     )
   }
 
   return (
     <Flex flexDirection="column" >
-        <Box backgroundColor={'white'} p={10}>
-        <Text fontSize='2rem'>Regalos:</Text>
-        <FormControl onSubmit={agregarRegalo} display={'flex'} flexWrap={['wrap','nowrap']}>
-          <Input type='text' name='regalo' id='regalo' autoCapitalize='on' autoComplete='off'/>
-          <Button  colorScheme={'blue'} w={['100%','30%','30%','30%']}  type='submit'>agregar</Button>
+        <Box backgroundColor={'white'} p={5}borderRadius={10}>
+        <Text fontSize='3rem' fontFamily={'Mountains of Christmas'} fontWeight={700}>Regalos:</Text>
+        <FormControl pt={4} display={'flex'} flexWrap={['wrap','nowrap']} gap={'8px'}>
+          <Input onChange={(e)=>setRegaloItem(() => e.target.value)} type='text' name='regalo' id='regalo' value={regaloItem} autoCapitalize='on' autoComplete='off'/>
+          <Button onClick={()=>agregarRegalo()}  colorScheme={'blue'} w={['100%','30%','30%','30%']}  type='submit'>agregar</Button>
         </FormControl>
         <ListRegalosComponent />
-        <Button w={'100%'} colorScheme={'red'}>Vaciar lista</Button>
+        <Button onClick={()=>setlistRegalos(prev => [])} w={'100%'} colorScheme={'red'}>Vaciar lista</Button>
         </Box>
     </Flex>
   );
