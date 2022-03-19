f61f54c10040c59a2834d9c259380b5e690c0389
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2022-03-19 17:01:19 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ function App() {
   const [regaloObject, setRegaloObject] = React.useState({regalo:'',cantidad:''});
   const [ListRegalos, setListaRegalos] = React.useState(JSON.parse(localStorage.getItem("LStorageRegalos")) || []);
   React.useEffect(() => {
-    console.log(ListRegalos)
     localStorage.setItem("LStorageRegalos", JSON.stringify(ListRegalos))
   }, [ListRegalos])
-
-
   function addRegalo(){
     const item = 
     {
@@ -24,22 +21,31 @@ function App() {
     }
     setRegaloObject({regalo:'',cantidad:''})
   };
-  function deleteRegalo(){};
+  function deleteRegalo(event){
+    let newArray = [];
+    let idEvent = event.nativeEvent.path[1].id;
+    setListaRegalos(prevData =>{
+      for(let i =0; i<prevData.length;i++){
+        if(prevData[i].id !== idEvent){newArray.push(prevData[i])};
+      }
+      return newArray
+    })
+  };
   function handleRegalo(event){
     setRegaloObject(prevData => {return {...prevData,[event.target.name]: event.target.value }})
   }
 
   function RegalosComponent(){
-    const lista = ListRegalos.map((item)=><ListItem key={item.id}>{item.regalo}</ListItem>)
+    const lista = ListRegalos.map((item)=><ListItem key={item.id} id={item.id} display={'flex'} flexWrap={'nowrap'} justifyContent={'space-between'}><p>{item.regalo}</p><Button size={'sm'} colorScheme={'red'} onClick={(e)=>deleteRegalo(e)}>X</Button></ListItem>)
     return(
-      <List>
+      <List spacing={'4px'} fontWeight={'500'}>
         {lista.length === 0 ?  <Center><Text color={'gray.500'}>No hay Regalos! Agregá algo!</Text></Center> : lista }
       </List>
     )
   }
   return (
     <Flex flexDirection="column" >
-        <Box bgColor={'whiteAlpha.800'} p={5} display={'flex'} gap={'8px'} flexWrap={'nowrap'} flexDir={'column'} borderRadius={10}>
+        <Box bgColor={'whiteAlpha.800'} p={5} display={'flex'} gap={'8px'} flexWrap={['wrap','nowrap']} flexDir={'column'} borderRadius={10}>
         <Text fontSize='3rem' fontFamily={'Mountains of Christmas'} fontWeight={700}>Regalos:</Text>
         <FormControl display={'flex'} flexWrap={['wrap','nowrap']} gap={'8px'}>
           <Box display={'flex'} gap={'8px'}>
